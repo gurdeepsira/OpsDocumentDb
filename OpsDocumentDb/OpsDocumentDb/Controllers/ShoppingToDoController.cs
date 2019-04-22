fa9acf0f6534d14e998a2788d229d8c6d80f57ce
Administrator <gurdipsira_1@yahoo.co.uk>
2019-04-22 13:44:35 -0700

    commit

---
+++ OpsDocumentDb/OpsDocumentDb/Controllers/ShoppingToDoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System.Security;
+
+namespace OpsDocumentDb.Controllers
+{
+    public class ShoppingToDoController : Controller
+    {
+        private const string EndpointUri = "&lt;YOUR-URI&gt;";
+        private const string AccessKey = "&lt;YOUR-PRIMARY-KEY&gt;";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ShoppingTodo todoItem)
+        {
+            using (var client = new DocumentClient(new Uri(EndpointUri), ToSecureString(AccessKey)))
+            {
+                const string DatabaseName = "todo";
+
+                // Check if Database exists
+                var database = client.CreateDatabaseQuery()
+                    .Where(db => db.Id == DatabaseName)
+        .AsEnumerable()
+        .FirstOrDefault();
+
+                if (database == null)
+                {
+                    database = await client.CreateDatabaseAsync(new Database
+                    {
+                        Id = DatabaseName
+                    });
+                }
+
+
+
+                const string CollectionName = "shopping";
+
+                // Check if collection already exists
+                DocumentCollection collection = client.CreateDocumentCollectionQuery(database.CollectionsLink)
+
+                                        .Where(c => c.Id == CollectionName)
+                            .AsEnumerable()
+                            .FirstOrDefault();
+
+                if (collection == null)
+                {
+                    // Create collection
+                    collection = await client.CreateDocumentCollectionAsync(
+
+                                                database.CollectionsLink,
+                                                new DocumentCollection { Id = CollectionName });
+                }
+
+                ResourceResponse<Document> createdItem = await
+         client.CreateDocumentAsync(collection.DocumentsLink, todoItem);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        private SecureString ToSecureString(string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                return null;
+            else
+            {
+                SecureString result = new SecureString();
+                foreach (char c in source.ToCharArray())
+                    result.AppendChar(c);
+                return result;
+            }
+        }
+    }
+}
---
+++ OpsDocumentDb/OpsDocumentDb/Models/ShoppingTodo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpsDocumentDb
+{
+    public class ShoppingTodo
+    {
+    
+            [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+            public string Id { get; set; }
+            [Newtonsoft.Json.JsonProperty(PropertyName = "item")]
+            public string Item { get; set; }
+            [Newtonsoft.Json.JsonProperty(PropertyName = "done")]
+            public bool Done { get; set; }
+            [Newtonsoft.Json.JsonProperty(PropertyName = "partitionkey")]
+            public int PartitionKey { get; set; }
+        
+    }
+}
